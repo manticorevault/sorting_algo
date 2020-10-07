0b1f296542d856b43bb0487a110a8edd1cccb180
manticorevault <artur_seilah@hotmail.com>
2020-10-07 15:30:25 +0100

    Implemented the first version to Bubble Sort an array of numbers. Need to fix it to also sort strings and linked lists

--- build/index.js
+++ build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("Hello!");
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var counter = 0; counter < length; counter++) {
+            for (var jCounter = 0; jCounter < length - counter - 1; jCounter++) {
+                if (this.collection[jCounter] >
+                    this.collection[jCounter + 1]) {
+                    var leftHand = this.collection[jCounter];
+                    this.collection[jCounter] = this.collection[jCounter + 1];
+                    this.collection[jCounter + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello!");
+class Sorter {
+	constructor(public collection: number[]) {}
+
+	sort(): void {
+		const { length } = this.collection;
+
+		for (let counter = 0; counter < length; counter++) {
+			for (
+				let jCounter = 0;
+				jCounter < length - counter - 1;
+				jCounter++
+			) {
+				if (
+					this.collection[jCounter] >
+					this.collection[jCounter + 1]
+				) {
+					const leftHand = this.collection[jCounter];
+					this.collection[jCounter] = this.collection[
+						jCounter + 1
+					];
+					this.collection[jCounter + 1] = leftHand;
+				}
+			}
+		}
+	}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+
+sorter.sort();
+
+console.log(sorter.collection);
